f1c37465ee8574e17981dadf8de82ee5dbca1c05
pichimanshi <himanshi0875@gmail.com>
2020-03-19 19:06:29 +0530

    In addTeacher and login(database json is added)

--- app/src/main/java/com/uuu/tutionattendace/Login.java
+++ app/src/main/java/com/uuu/tutionattendace/Login.java
@@ -2,17 +2,27 @@ package com.uuu.tutionattendace;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Login extends AppCompatActivity implements View.OnClickListener {
 
+    ProgressBar p;
     Button go,forgotpass;
     TextInputLayout username,password;
     String user,pass;
@@ -42,6 +52,7 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
                 user= username.getEditText().getText().toString();
                 pass=password.getEditText().getText().toString();
                 checkDataOfLogin();
+//                new LoginDb().execute();
                 break;
 
             case R.id.forgotPass:
@@ -72,4 +83,58 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         CharSequence str=text.getEditText().getText().toString();//it canv read empty string//important
         return TextUtils.isEmpty(str);
     }
+
+    public  class LoginDb extends AsyncTask<Void,Void,String> {
+        String result="";
+
+        @Override
+        protected void onPreExecute() {
+            username.setError(null);
+            password.setError(null);
+            p=findViewById(R.id.TprogressBar);
+            p.setVisibility(View.VISIBLE);
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            try {
+                //Ip ->ip
+                URL url = new URL("http:// IP  /android/login.php?email="+user+"&pswd="+pass);
+                InputStream stream=url.openConnection().getInputStream();
+                InputStreamReader ir = new InputStreamReader(stream);
+                BufferedReader br = new BufferedReader(ir);
+                String line="";
+
+                while ((line=br.readLine())!=null){
+                    result=result+line+"\n";
+                }
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            p.setVisibility(View.INVISIBLE);
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            String data=s.trim();
+            if(data.matches("true")){
+                Toast.makeText(getApplicationContext(), "Valid User", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(getApplicationContext(), "InValid User !!", Toast.LENGTH_SHORT).show();
+            }
+            super.onPostExecute(s);
+        }
+    }
+
 }
